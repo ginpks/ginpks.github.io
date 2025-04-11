481075b21ae31a161be4db2d85a0532a9f367758
ginpks <ginhpark@gmail.com>
2025-04-11 01:13:23 -0400

    added blog

--- scripts.js
+++ scripts.js
@@ -9,3 +9,17 @@ back.addEventListener('click', () => {
     document.querySelector('.text').style.display = "block";
     document.querySelector('.projects').style.display = "none";
 })
+
+
+const blog = document.getElementById("info4");
+blog.addEventListener('click', () => {
+    document.querySelector('.text').style.display = "none";
+    document.querySelector('.blog').style.display = "block";
+})
+
+const back2= document.getElementById("back2");
+back2.addEventListener('click',() => {
+    document.querySelector('.text').style.display = "block";
+    document.querySelector('.blog').style.display = "none";
+})
+
